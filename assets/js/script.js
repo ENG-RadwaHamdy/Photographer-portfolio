2a4a982c9cbbd3e82409ff86c84e01551028837c
r.hamdy <r.hamdy@solutionplus.net>
2025-03-02 15:39:13 +0200

    feat:Implement mobile navigation toggle and header scroll effect

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,42 @@ window.addEventListener("load", function(){
     loadingElement.classList.add("loaded");
     this.document.body.classList.remove("active");
 })
+
+// mobile nav toggle
+
+const [navTogglers, navLinks, navbar, overlay] = [
+    document.querySelectorAll("[data-nav-toggler"),
+    document.querySelectorAll("[data-nav-link]"),
+    document.querySelector("[data-navbar]"),
+    document.querySelector("[data-overlay]")
+];
+
+const toggleNav = function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("active");
+}
+
+addEventsOnElements(navTogglers, "click", toggleNav);
+
+const closeNav = function () {
+    navbar.classList.remove("active");
+    overlay.classList.remove("active");
+    document.body.classList.remove("active");
+}
+
+addEventsOnElements(navLinks, "click", closeNav);
+
+//header
+
+const header = document.querySelector("[data-header]");
+
+const activeElementOnScroll = function () {
+    if(window.screenY > 50){
+        header.classList.add("active");
+    }else{
+        header.classList.remove("active");
+    }
+}
+
+window.addEventListener("scroll", activeElementOnScroll)
