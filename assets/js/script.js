b7a122c817d611ac79d304cf113edc392508cc80
r.hamdy <r.hamdy@solutionplus.net>
2025-03-05 01:22:03 +0200

    feat:Add back to top button with scroll percentage display

--- assets/js/script.js
+++ assets/js/script.js
@@ -135,3 +135,25 @@ const setLetterEffect = function () {
 
 // call the letter effect function after window loaded
 window.addEventListener("load", setLetterEffect);
+
+
+// back to top button
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+
+window.addEventListener("scroll", function (){
+    const bodyHeight = document.body.scrollHeight;
+    const windowHeight = window.innerHeight;
+
+    const scrollEndPos = bodyHeight - windowHeight;
+    const totalScrollPercent = (this.window.scrollY / scrollEndPos) * 100;
+
+    backTopBtn.textContent = `${totalScrollPercent.toFixed(0)}%`;
+
+    //visible back top top btn when scrolled 5% of the page
+    if(totalScrollPercent > 5){
+        backTopBtn.classList.add("show");
+    }else{
+        backTopBtn.classList.remove("show");
+    }
+})
