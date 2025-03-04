4f0e6040aa131ba9bce00e4c1341997e2ee021a8
r.hamdy <r.hamdy@solutionplus.net>
2025-03-05 01:37:53 +0200

    feat:Add scroll reveal effect and custom cursor functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -156,4 +156,27 @@ window.addEventListener("scroll", function (){
     }else{
         backTopBtn.classList.remove("show");
     }
-})
+});
+
+
+// scroll reveal
+
+const revealElements = document.querySelectorAll("[data-reveal]");
+
+const scrollReveal = function() {
+    for(let i=0; i < revealElements.length; i++){
+        const elementsInScreen = revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.15;
+
+
+        if(elementsInScreen){
+            revealElements[i].classList.add("revealed");
+        }else{
+            revealElements[i].classList.remove("revealed")
+        }
+    }
+}
+
+
+window.addEventListener("scroll", scrollReveal);
+
+scrollReveal();
